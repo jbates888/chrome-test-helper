8ca5529f25dd4c882f70b90deb115d1ca761bcfc
jbates888 <jackbates555@gmail.com>
2023-04-23 22:46:40 -0500

    remove boiler code

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <button class="btn">{{ text }}</button>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
